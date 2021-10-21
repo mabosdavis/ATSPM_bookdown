8d81610956ad73e17f2054b877014949dd7afce9
BYU Traffic Lab <byutraffic@utah.gov>
2021-10-20 18:29:41 -0600

    Update to the atspm_594R.R file with functions to create base tibble

---
+++ R/atsmp_594R.R
@@ -0,0 +1,38 @@
+
+# Install tidyverse if needed
+if (!require("tidyverse")) install.packages("tidyverse")
+
+# Load tidyverse library
+library(tidyverse)
+
+# Read in PCD csv
+PCD <- read_csv("data/PCD.csv") %>%
+  select( -IsProtectedPhase, -TotalDelay, -ApproachId)
+
+# Read in SF csv
+SF <- read_csv("data/SplitFails.csv") %>%
+  filter(IsProtectedPhase == "1") %>%
+  select(-IsProtectedPhase, -GreenOccupancySum, -RedOccupancySum,
+         -GreenTimeSum, -RedTimeSum, -ApproachId)
+# Read in YR csv
+YR <- read_csv("data/YellowRedActivations.csv") %>%
+  filter(IsProtectedPhase == 1) %>%
+  mutate(RVCycles = Cycles) %>%
+  select(-IsProtectedPhase, -SevereRedLightViolations, -YellowActivations,
+       -ViolationTime, -ApproachId, -Cycles)
+
+# Read in BinStartTime csv
+BinStartTime <- read_csv("data/BinStartTimes_2020.csv")
+
+# Read in Signal ID csv  
+SignalId <- read_csv("data/SignalId.csv")
+
+# Create a Phase Number 2&6 tibble
+PhaseNumber <- tibble(PhaseNumber = c(2,6))
+
+base_tibble <-
+    expand_grid(
+      BinStartTime,
+      SignalId,
+      PhaseNumber
+    )
