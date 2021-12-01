bf21aada32da8cc804166d5b73f8c8bb30511da3
Matthew Davis <mabosdavis@gmail.com>
2021-11-30 19:17:59 -0700

    Created a new file to start regression analysis on Volume data. Most of the file is copied from the top of the Volume.R file

---
+++ R/Volume_Regression.R
@@ -0,0 +1,74 @@
+# Create Volume df
+library(tidyverse)
+library(lubridate)
+library(data.table)
+
+Volume_pre<- read_csv("data/TrafficVolume.csv")
+
+vol <- Volume_pre %>%
+  select(-Movement, -LaneNumber) %>% data.table()
+
+b <- vol[, lapply(.SD, sum), by=list(BinStartTime, SignalId, ApproachId, PhaseNumber)] %>%
+  as_tibble()
+
+# Write Peak identifier    
+c <- b %>% mutate(Volume_hour = Volume*4,
+                  hour = lubridate::hour(BinStartTime),
+                  timeperiod = case_when(
+                    hour >= 7 & hour <= 9 ~ "AMPeak",
+                    hour >= 12 & hour <= 14 ~ "MidDay",
+                    hour >= 16 & hour <= 18 ~ "PMPeak",
+                    TRUE ~ "OffPeak"))
+# Filter by Phase Number, only AM and PM Peaks, and after November 12/2017
+d <- c %>% 
+  filter(PhaseNumber%in% c(2,6), 
+         timeperiod %in% c("AMPeak", "PMPeak"),
+         BinStartTime >= "2017-11-12")
+
+# Write rds file
+write_rds(d, "data/Volume.rds")
+
+
+
+
+
+# Start HERE unless rebuilding Volume file
+library(tidyverse)
+library(stats)
+library(lubridate)
+
+# Read Volume file in
+Volume <- read_rds("data/Volume.rds")
+
+# Make a list of Signals for reference
+Signal_List <- c(unique(Volume$SignalId)) %>% 
+  as.tibble() %>%
+  rename(SignalId = value)
+
+# Read in Signal Names csv
+Signals_Unique <- read_csv("data/Signals_Joined_Unique.csv") %>%
+  select(SignalId, StreetName, PrimaryName, SecondaryName)
+
+#Left Join the Signals from Volume and from Signals_Unique
+Signals_Unique <- left_join(Signal_List, Signals_Unique, by = 'SignalId')
+
+#Left Join the Volume and Signals Unique csv for Corridor Names
+Volume <- left_join(Volume, Signals_Unique, by = 'SignalId')
+
+
+# Extract day from date
+Volume <- mutate(Volume, day = lubridate::wday(BinStartTime))
+
+# Filter just Tuesday, Wednesday and Thursday
+Volume <- filter(Volume, day >= 3, day <= 5)
+
+#Filter workable signals
+Workable_Signals <- c(4301, 4090, 4704, 4705, 6303, 6304, 6307)
+Volume <- filter(Volume, SignalId%in% Workable_Signals)
+
+
+# Build the Pre-Covid df
+Vol_PC <- filter(Volume, BinStartTime < "2020-03-12") #, BinStartTime > "2019-09-12")
+
+# Build the Covid df
+Vol_C <- filter(Volume, BinStartTime >= "2020-03-12")
